b30573f73b47a074222ee66ec989ededbfec4e48
Sofia <sofleitas09@gmail.com>
2024-08-25 21:59:23 -0300

    Solución click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
+function saludar(event) {
+    alert('Hola!');
+    event.stopPropagation()
+}
+
 document.querySelector("div").addEventListener('click', function() {
     alert('Hola! Soy el div')
 })
